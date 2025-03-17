bac4cb5c8e544f3cbfe7cf6cde5c7b4949733c18
Srinath <dhumnorsrinath06052002@gmail.com>
2025-03-17 12:42:49 +0530

    Update settings.py

--- DevConnect/connect/connect/settings.py
+++ DevConnect/connect/connect/settings.py
@@ -17,9 +17,9 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend1', 'dist')  # React build folder
+    os.path.join(BASE_DIR, 'frontend1/tailwindcss4/dist')  # React build folder
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
